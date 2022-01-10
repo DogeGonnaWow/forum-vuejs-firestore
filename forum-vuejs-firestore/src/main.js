575efa786483440b82675505036d9dbcee437c1f
DogeGonnaWow <gentilhomme.sempai@gmail.com>
2022-01-10 14:39:55 +0100

    firebase init

--- forum-vuejs-firestore/src/main.js
+++ forum-vuejs-firestore/src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from "firebase/compat";
+import firebaseConfig from './plugins/firebase-config'
 
 Vue.config.productionTip = false
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
